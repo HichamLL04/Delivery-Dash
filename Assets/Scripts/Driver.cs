eeef577d5f38cfe94ae591b1bc917aa4efae32ca
Kaiser <hichamlaaouni52@gmail.com>
2025-12-08 18:48:13 +0100

    11 SerializeField

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float rotateSpeed = 0.5f;
+    [SerializeField] float moveSpeed = 0.05f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,7 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,0,0.5f);
-        transform.Translate(0,0.02f,0);
+        transform.Rotate(0,0,rotateSpeed);
+        transform.Translate(0,moveSpeed,0);
     }
 }

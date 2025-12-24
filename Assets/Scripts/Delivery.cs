da17ee44741f7319781f9456f13127b92125b290
Kaiser <hichamlaaouni52@gmail.com>
2025-12-24 22:51:41 +0100

    Update Delivery.cs
    Evitar entregar el mismo paquete

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -2,17 +2,21 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+
+    bool hasPackage = false;
     void OnTriggerEnter2D(Collider2D other)
     {
         // Ahora se usa "other" no "collision" a diferencia del video
-        if (other.CompareTag("Paquete"))
+        if (other.CompareTag("Paquete") && !hasPackage)
         {
             Debug.Log("Paquete " + other.gameObject.name + " recogido.");
+            hasPackage = true;
         }
 
-        if (other.CompareTag("Cliente"))
+        if (other.CompareTag("Cliente") && hasPackage)
         {
             Debug.Log("Cliente " + other.gameObject.name + " ha recibido el paquete.");
+            hasPackage = false;
         }
     }
 }
